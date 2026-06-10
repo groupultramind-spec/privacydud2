9fa6e3d7d0fbca403b1af6bc89059920442cc471
Group Ultramind <groupultramind@gmail.com>
2026-06-10 05:10:52 -0300

    fix: disable display_errors to prevent PHP deprecation warning headers block

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+ini_set('display_errors', 0);
 date_default_timezone_set('America/Sao_Paulo');
 /**
  * Configurações do Sistema de Pagamento PIX
